4c63ba2c168d93cc10e166361d395afebc265140
Mikhail Ilin <m.ilin.fr@gmail.com>
2019-08-31 09:38:09 +0200

    initial commit

---
+++ gv_compta.py
@@ -0,0 +1,105 @@
+import tkinter
+from tkinter import Label, Button, Entry, Checkbutton, OptionMenu, Canvas, Frame, Toplevel
+from tkinter import StringVar, IntVar
+from tkinter.messagebox import showinfo
+import openpyxl
+from openpyxl import Workbook, load_workbook
+import datetime
+
+'''Below module allows us to interact with Windows files.'''
+import os
+
+'''below 3 lines allows script to check the directory where it is executed, so it knows where to crete the excel file. I copied the whole block from stack overflow'''
+abspath = os.path.abspath(__file__)
+current_directory = os.path.dirname(abspath)
+os.chdir(current_directory)
+
+def get_file_names_in_script_directory():
+    file_names = []
+    for root, dirs, files in os.walk(current_directory):
+        for filename in files:
+            file_names.append(filename)
+    return file_names
+
+
+def get_existing_excel_names():
+    '''Each excel file stands for type of work'''
+    existing_excel_names = []
+    for file_name in get_file_names_in_script_directory():
+        if "GV compta" in file_name:
+            existing_excel_names.append(file_name)
+    return existing_excel_names
+
+
+
+
+def get_date_yyyy_mm_dd():
+    return str(datetime.date.today())
+
+
+def get_current_year():
+    #current_month = datetime.datetime.now().strftime('%B') #extracts the name of the current month
+    current_year = datetime.datetime.now().strftime('%Y') #extracts the current year
+    return str(current_year)
+
+
+def generate_the_excel_file_name_with_current_year_in_name():
+    return (get_current_year() + " sandbox.xlsx")
+
+
+def check_if_excel_file_is_there():
+    return generate_the_excel_file_name_with_current_year_in_name() in get_existing_excel_names()
+
+
+def create_excel_file_if_it_was_not_there():
+    if not check_if_excel_file_is_there():
+        wb = Workbook()
+        ws = wb.active
+        ws.title = "first"
+        ws.cell(row=1, column=1, value="name")
+        ws.cell(row=1, column=2, value="last date")
+        ws.cell(row=1, column=3, value="last payment")
+        ws.cell(row=1, column=4, value="total payment")
+        wb.save(generate_the_excel_file_name_with_current_year_in_name())
+
+
+def find_the_next_empty_row(ws):
+    row_index = 1
+    cell_to_check = ws.cell(row = row_index, column = 1)
+    while cell_to_check.value != None:
+        row_index = row_index + 1
+        cell_to_check = ws.cell(row = row_index, column = 1)
+    return row_index
+
+
+def create_missing_sheet(name, excel_workbook):
+    print("creating missing sheet " + name)
+    if name not in excel_workbook.sheetnames:
+        excel_workbook.create_sheet(name)
+        ws = excel_workbook[name]
+        ws.cell(row=1, column=1, value="last date")
+        ws.cell(row=1, column=2, value="last payment")
+
+def button_pressed():
+    pass
+
+
+def open_work_selection_window():
+    window_start_new_work = Toplevel()
+    x = main_window_of_gui.winfo_x()
+    y = main_window_of_gui.winfo_y()
+    w = main_window_of_gui.winfo_width()
+    h = main_window_of_gui.winfo_height()  
+    window_start_new_work.geometry("%dx%d+%d+%d" % (w, h, x, y))
+    window_start_new_work.title("Type de traveaux")
+    window_start_new_work.wm_attributes("-topmost", 1)
+
+
+main_window_of_gui = tkinter.Tk()
+main_window_of_gui.title("sandbox")
+main_window_of_gui.wm_attributes("-topmost", 1)
+
+button_start_new_work = Button(main_window_of_gui, text="Nouvelle facture", width=20, height=3, command=open_work_selection_window)
+button_start_new_work.grid(row=0, column=0)
+
+main_window_of_gui.mainloop()
